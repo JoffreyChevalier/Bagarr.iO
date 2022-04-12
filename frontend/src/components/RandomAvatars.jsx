a1869ce71763943971fbf5f3734088ad8a31d675
Joffrey Chevalier <100203396+JoffreyChevalier@users.noreply.github.com>
2022-04-12 09:25:05 +0200

    Random avatar component (#3)
    clean the react vite app default page, add api.js in services, create the components RandomAvatar.jsx and add it to app.jsx, cleaned the app.css

---
+++ frontend/src/components/RandomAvatars.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { useState } from "react";
+
+const refresh = () => {
+  return `https://avatars.dicebear.com/api/avataaars/:${Math.floor(
+    Math.random() * 1000000
+  )}.svg`;
+};
+
+function RandomAvatar() {
+  const [newAvatar, setNewAvatar] = useState(refresh());
+
+  return (
+    <div>
+      <img width="200" height="200" src={newAvatar} alt="Your avatar" />
+      <button
+        type="button"
+        onClick={() => {
+          setNewAvatar(refresh());
+        }}
+      >
+        Randomize
+      </button>
+    </div>
+  );
+}
+
+export default RandomAvatar;
