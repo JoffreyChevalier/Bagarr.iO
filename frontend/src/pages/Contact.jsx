9f2bbf1f19af5eab9ae482b7e343dfd9b5619587
CarolinaDelerue <99035993+CarolinaDelerue@users.noreply.github.com>
2022-04-20 12:40:53 +0200

    Boutons (#12)
    * installation de Tailwind
    
    * composant bouton fix
    
    * fin boutons 2
    
    * boutons fin 4
    
    Co-authored-by: Joffrey Chevalier <100203396+JoffreyChevalier@users.noreply.github.com>

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,33 @@
+import TButton from "@components/TButton";
+import {
+  ArrowRightIcon as ArrowRightIconOutline,
+  ArrowLeftIcon as ArrowLeftIconOutline,
+} from "@heroicons/react/outline";
+
+// Exemples de boutons à personnaliser
 function Formulaire() {
   return (
     <div>
       <h1>ICI la page du Formulaire</h1>
+      <TButton variant="transparent">
+        <ArrowRightIconOutline className="h-5 w-5 text-[#70A288] " />
+      </TButton>
+
+      <TButton
+        variant="primary"
+        rounded
+        buttonClass="text-[##FFFFFF] h-24 w-24 text-xl"
+      >
+        Prêt ?
+      </TButton>
+
+      <TButton variant="transparent">
+        <ArrowLeftIconOutline className="h-5 w-5 text-[#70A288]" />
+      </TButton>
+
+      <TButton variant="primary" buttonClass="text-[  #FFFFFF] text-sm">
+        Choisi ton avatar
+      </TButton>
     </div>
   );
 }
