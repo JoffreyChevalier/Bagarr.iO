ab84d48cbb6633b58d91e2f1725401d78792fe93
Joffrey Chevalier <chevalier.joffrey@live.fr>
2022-04-21 14:00:30 +0200

    add modalHint for Homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,20 @@
+import ModalHint from "@components/ModalHint";
+
 function Home() {
   return (
-    <div>
-      <h1>Bienvenue sur Bagarr.IO !</h1>
-      <h3>
-        Devenez maître du tournoi grâce à cette application, affrontez vous :
-        joueur contre joueur ou en groupe ! Mettez en jeu des récompenses pour
-        les gagnants et des gages pour les perdants !
-      </h3>
-    </div>
+    <>
+      <div className="flex justify-end mr-8">
+        <ModalHint />
+      </div>
+      <div>
+        <h1>Bienvenue sur Bagarr.IO !</h1>
+        <h3>
+          Devenez maître du tournoi grâce à cette application, affrontez vous :
+          joueur contre joueur ou en groupe ! Mettez en jeu des récompenses pour
+          les gagnants et des gages pour les perdants !
+        </h3>
+      </div>
+    </>
   );
 }
 export default Home;
