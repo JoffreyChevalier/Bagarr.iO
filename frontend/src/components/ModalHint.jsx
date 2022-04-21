323c45f9c0dd2b98e5b45a7efa17d339d668eb83
Joffrey Chevalier <chevalier.joffrey@live.fr>
2022-04-21 14:00:45 +0200

    add modalHint for Homepage

--- frontend/src/components/ModalHint.jsx
+++ frontend/src/components/ModalHint.jsx
@@ -5,7 +5,10 @@ export default function ModalHint() {
     <div>
       <Popup
         trigger={
-          <button className="button focus:outline-none transition duration-150 ease-in-out hover:bg-indigo-600 bg-[#BA8208] rounded text-white px-4 py-1 text-md">
+          <button
+            type="button"
+            className="button focus:outline-none transition duration-150 ease-in-out hover:bg-indigo-600 bg-[#BA8208] rounded text-white px-4 py-1 text-md"
+          >
             {" "}
             Première fois ?{" "}
           </button>
@@ -17,6 +20,7 @@ export default function ModalHint() {
           <div>
             <div className="container mx-auto w-11/12 md:w-2/2 max-w-xxl">
               <div className="modal relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
+                {/* eslint-disable-next-line */}
                 <div
                   className="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out"
                   onClick={close}
@@ -64,6 +68,7 @@ export default function ModalHint() {
                 </div>
                 <div className="actions flex mt-8 items-center justify-start w-full">
                   <button
+                    type="button"
                     className="button focus:outline-none transition duration-150 ease-in-out hover:bg-indigo-600 bg-[#BA8208] rounded text-white px-8 py-2 text-sm"
                     onClick={() => {
                       close();
