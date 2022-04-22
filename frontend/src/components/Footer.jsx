8d097111a9c0ffe1b96f42fd1e34cdaf8b78fef8
Virginie <93268361+Virginie-Grrf@users.noreply.github.com>
2022-04-22 09:47:17 +0200

    Vivi branch (#25)
    * wip
    
    * texte centré
    
    * changement de couleur dans le footer
    
    * wip
    
    * amelioration du footer et de la phrase d'acroche
    
    * add pack frontend
    
    * wip
    
    * footer responsive

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,13 +4,13 @@ function Footer() {
   return (
     <footer className="footer">
       <ul className="formlegales">
-        <li>© 2022 Copyright.</li>
         <li>
           <NavLink to="/formulaires">Contact</NavLink>
         </li>
         <li>
           <NavLink to="/mentionslegales">Mentions Légales</NavLink>
         </li>
+        <li>© 2022 Copyright.</li>
       </ul>
     </footer>
   );
