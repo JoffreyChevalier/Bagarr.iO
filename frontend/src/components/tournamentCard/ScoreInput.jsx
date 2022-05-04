01c39c9867f50d71d402af36b254929aad18592c
Joffrey Chevalier <chevalier.joffrey@live.fr>
2022-05-04 16:56:34 +0200

    change input in Player Card

--- frontend/src/components/tournamentCard/ScoreInput.jsx
+++ frontend/src/components/tournamentCard/ScoreInput.jsx
@@ -1,9 +1,34 @@
-import Input from "@components/Input";
-
-function SetScoreButton() {
+function SetScoreButton({ name, onChange }) {
   return (
-    <div>
-      <Input placeholder="Score du Joueur" />
+    <div className="flex button_players">
+      <div className="flex justify-center">
+        <div className="mb-3 xl:w-40">
+          {/* On fait passer les données la saisie de l'utilisateur via la props onChange */}
+          <input
+            value={name}
+            onChange={(event) => onChange(event.target.value)}
+            placeholder="Score"
+            className="form-select appearance-none
+            block
+            w-16
+            px-1
+            py-1
+            text-base
+            font-standard_font
+            text-black
+            placeholder:text-slate-400
+            bg-white bg-clip-padding bg-no-repeat
+            border border-solid border-border_button
+            rounded
+            transition
+            ease-in-out
+            m-0
+            text-center
+            focus:text-black focus:bg-slate-100 focus:border-border_button focus:outline-none focus:text-center"
+            aria-label="Default select example"
+          />
+        </div>
+      </div>
     </div>
   );
 }
